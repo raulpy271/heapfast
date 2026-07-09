7769b5d9b4679d66aa1df7f80f2d6ccc9eef661c
Raul <raulrufino0@gmail.com>
2026-07-08 23:04:22 -0300

    Add heapify algorithm

--- heapfast.go
+++ heapfast.go
@@ -1,12 +1,54 @@
 package main
 
+import (
+	"math"
+)
+
 type Item struct {
-	key float64;
+	key   float64
 	value any
 }
 
 type Heap struct {
-	items []Item
+	items  [2]Item
+	length uint
+}
+
+func left(i uint) uint {
+	// i * 2 + 1
+	return (i << 1) + 1
+}
+
+func right(i uint) uint {
+	// i * 2 + 2
+	return (i << 1) + 2
+}
+
+func parent(i uint) uint {
+	return uint(math.Floor((float64(i) - 1) / 2))
+}
+
+func (h *Heap) heapify(i uint) {
+	var l, r, largest uint
+	for i < h.length {
+		l = left(i)
+		r = right(i)
+		if l < h.length && h.items[i].key < h.items[l].key {
+			largest = l
+		} else {
+			largest = i
+		}
+		if r < h.length && h.items[largest].key < h.items[r].key {
+			largest = r
+		}
+		if i == largest {
+			break
+		} else {
+			h.items[i], h.items[largest] = h.items[largest], h.items[i]
+			i = largest
+		}
+	}
+
 }
 
 func (h *Heap) AddItem(i Item) {
--- heapfast_test.go
+++ heapfast_test.go
@@ -16,8 +16,53 @@ func TestPopItem(t *testing.T) {
 	if i.key != r.key {
 		t.Error(i, r)
 	}
-	if len(i.value) != len(r.value) {
-		t.Error(i, r)
-	}
+}
 
+func TestLeft(t *testing.T) {
+	for i := range uint(10) {
+		if left(i) != (i*2 + 1) {
+			t.Error(left(i), i*2+1)
+		}
+	}
+}
+
+func TestRight(t *testing.T) {
+	for i := range uint(10) {
+		if right(i) != (i*2 + 2) {
+			t.Error(right(i), i*2+2)
+		}
+	}
+}
+
+func TestParent(t *testing.T) {
+	m := map[uint]uint{1: 0, 2: 0, 3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 9: 4}
+	for i, v := range m {
+		if parent(i) != v {
+			t.Error(parent(i), v)
+		}
+	}
+}
+
+func TestHeapifyOneNode(t *testing.T) {
+	arr := []Item{{10, nil}, {12, nil}, {22, nil}}
+	h := Heap{arr, 1}
+	h.heapify(0)
+	if h.items[0].key != 10 {
+		t.Error(h)
+	}
+}
+
+func TestHeapifyThreeNode(t *testing.T) {
+	arr := []Item{{10, nil}, {12, nil}, {22, nil}}
+	h := Heap{arr, 3}
+	h.heapify(0)
+	if h.items[0].key != 22 {
+		t.Error(h)
+	}
+	if h.items[1].key != 12 {
+		t.Error(h)
+	}
+	if h.items[2].key != 10 {
+		t.Error(h)
+	}
 }
