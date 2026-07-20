b6371262ff52be1e9495f8f4471d681198a1c4fb
Raul <raulrufino0@gmail.com>
2026-07-20 20:49:19 -0300

    Rename heapfast file to heapmax

--- heapfast/heapfast.go
+++ heapfast/heapfast.go
@@ -12,9 +12,6 @@ type Heap[T cmp.Ordered] struct {
 	Length  uint
 }
 
-type HeapMax[T cmp.Ordered] Heap[T]
-type HeapMin[T cmp.Ordered] Heap[T]
-
 func left(i uint) uint {
 	// i * 2 + 1
 	return (i << 1) + 1
@@ -29,69 +26,3 @@ func parent(i uint) uint {
 	return uint(math.Floor((float64(i) - 1) / 2))
 }
 
-func (h *HeapMax[T]) heapify(i uint) {
-	var l, r, largest uint
-	for i < h.Length {
-		l = left(i)
-		r = right(i)
-		if l < h.Length && h.Records[i][0] < h.Records[l][0] {
-			largest = l
-		} else {
-			largest = i
-		}
-		if r < h.Length && h.Records[largest][0] < h.Records[r][0] {
-			largest = r
-		}
-		if i == largest {
-			break
-		} else {
-			h.Records[i], h.Records[largest] = h.Records[largest], h.Records[i]
-			i = largest
-		}
-	}
-}
-
-func BuildMaxHeap[T cmp.Ordered](records []Record[T]) *HeapMax[T] {
-	heap := &HeapMax[T]{records, uint(len(records))}
-	i := int(math.Floor((float64(len(records)) / 2) - 1))
-	for ; i >= 0; i-- {
-		heap.heapify(uint(i))
-	}
-	return heap
-}
-
-func SortMax[T cmp.Ordered](records []Record[T]) []Record[T] {
-	heap := BuildMaxHeap(records)
-	for i := heap.Length - 1; i > 0; i-- {
-		heap.Records[i], heap.Records[0] = heap.Records[0], heap.Records[i]
-		heap.Length--
-		heap.heapify(0)
-	}
-	return heap.Records
-}
-
-func (h *HeapMax[T]) AddItem(record Record[T]) {
-	if int(h.Length) == len(h.Records) {
-		h.Records = append(h.Records, record)
-	} else {
-		h.Records[h.Length] = record
-	}
-	i := h.Length
-	h.Length++
-	for i > 0 && h.Records[parent(i)][0] < h.Records[i][0] {
-		h.Records[i], h.Records[parent(i)] = h.Records[parent(i)], h.Records[i]
-		i = parent(i)
-	}
-}
-
-func (h HeapMax[T]) Top() Record[T] {
-	return h.Records[0]
-}
-
-func (h *HeapMax[T]) PopItem() Record[T] {
-	i := h.Records[0]
-	h.Records[0] = h.Records[h.Length-1]
-	h.Length--
-	h.heapify(0)
-	return i
-}
