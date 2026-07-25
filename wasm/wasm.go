6093e6216d4a50b34fb5cfb90743e0351413dcd1
Raul <raulrufino0@gmail.com>
2026-07-24 21:49:45 -0300

    Export functions to create heaps with different types

--- wasm/wasm.go
+++ wasm/wasm.go
@@ -9,63 +9,97 @@ import (
 	"github.com/raulpy271/heapfast/internal"
 )
 
-func mean(this js.Value, args []js.Value) any {
-	if len(args) == 0 || args[0].Length() == 0 {
-		return js.ValueOf(0)
-	}
-	arr := args[0]
-	n := arr.Length()
-	sum := 0.0
-	for i := range n {
-		sum += arr.Index(i).Float()
-	}
-	return js.ValueOf(sum / float64(n))
+func addMethods[R uint32 | int32 | uint64 | int64, T heapfast.Heap[R]](heaps []T, heapref map[string]any) {
+	heapref["add"] = js.FuncOf(func(this js.Value, args []js.Value) any {
+		v := this.Get("pos").Int()
+		h := heaps[v]
+		h.AddItem(heapfast.Record[R]{R(args[0].Int()), R(args[1].Int())})
+		return nil
+	})
+	heapref["addBulk"] = js.FuncOf(func(this js.Value, args []js.Value) any {
+		if !args[0].InstanceOf(js.Global().Get("Array")) || !args[1].InstanceOf(js.Global().Get("Array")) {
+			panic("O primeiro parametro deve ser um array")
+		}
+		v := this.Get("pos").Int()
+		h := heaps[v]
+		keys := args[0]
+		values := args[1]
+		l := keys.Length()
+		for i := range l {
+			h.AddItem(heapfast.Record[R]{R(keys.Index(i).Int()), R(values.Index(i).Int())})
+		}
+		return nil
+	})
+	heapref["pop"] = js.FuncOf(func(this js.Value, args []js.Value) any {
+		v := this.Get("pos").Int()
+		h := heaps[v]
+		i := h.PopItem()
+		return []any{i[0], i[1]}
+	})
+
 }
 
 func main() {
-	heaps := make([]heapfast.Heap[uint32], 0, 20)
-	js.Global().Set("NewHeap", js.FuncOf(func(this js.Value, args []js.Value) any {
-		h := heapfast.Heap[uint32]{}
-		heaps = append(heaps, h)
-		pos := len(heaps) - 1
+	heaps32 := make([]heapfast.Heap[int32], 0, 20)
+	heaps64 := make([]heapfast.Heap[int64], 0, 20)
+	js.Global().Set("NewHeapMax", js.FuncOf(func(this js.Value, args []js.Value) any {
+		h := heapfast.HeapMax[int32]{}
+		heaps32 = append(heaps32, &h)
+		pos := len(heaps32) - 1
 		heapref := make(map[string]any)
 		heapref["pos"] = pos
-		heapref["add"] = js.FuncOf(func(this js.Value, args []js.Value) any {
-			v := this.Get("pos").Int()
-			h := &heaps[v]
-			h.AddItem(heapfast.Record[uint32]{uint32(args[0].Int()), uint32(args[1].Int())})
-			return nil
-		})
-		heapref["addBulk"] = js.FuncOf(func(this js.Value, args []js.Value) any {
-			if !args[0].InstanceOf(js.Global().Get("Array")) || !args[1].InstanceOf(js.Global().Get("Array")) {
-				panic("O primeiro parametro deve ser um array")
-			}
-			v := this.Get("pos").Int()
-			h := &heaps[v]
-			keys := args[0]
-			values := args[1]
-			l := keys.Length()
-			for i := range l {
-				h.AddItem(heapfast.Record[uint32]{uint32(keys.Index(i).Int()), uint32(values.Index(i).Int())})
-			}
-			return nil
-		})
-		heapref["pop"] = js.FuncOf(func(this js.Value, args []js.Value) any {
-			v := this.Get("pos").Int()
-			h := &heaps[v]
-			i := h.PopItem()
-			return []any{i[0], i[1]}
-		})
+		addMethods[int32, heapfast.Heap[int32]](heaps32, heapref)
 		return heapref
 	}))
-	js.Global().Set("Heapsort", js.FuncOf(func(this js.Value, args []js.Value) any {
+	js.Global().Set("NewHeapMin", js.FuncOf(func(this js.Value, args []js.Value) any {
+		h := heapfast.HeapMin[int32]{}
+		heaps32 = append(heaps32, &h)
+		pos := len(heaps32) - 1
+		heapref := make(map[string]any)
+		heapref["pos"] = pos
+		addMethods[int32, heapfast.Heap[int32]](heaps32, heapref)
+		return heapref
+	}))
+	js.Global().Set("NewHeapMax64", js.FuncOf(func(this js.Value, args []js.Value) any {
+		h := heapfast.HeapMax[int64]{}
+		heaps64 = append(heaps64, &h)
+		pos := len(heaps64) - 1
+		heapref := make(map[string]any)
+		heapref["pos"] = pos
+		addMethods[int64, heapfast.Heap[int64]](heaps64, heapref)
+		return heapref
+	}))
+	js.Global().Set("NewHeapMin64", js.FuncOf(func(this js.Value, args []js.Value) any {
+		h := heapfast.HeapMin[int64]{}
+		heaps64 = append(heaps64, &h)
+		pos := len(heaps64) - 1
+		heapref := make(map[string]any)
+		heapref["pos"] = pos
+		addMethods[int64, heapfast.Heap[int64]](heaps64, heapref)
+		return heapref
+	}))
+	js.Global().Set("HeapsortAsc", js.FuncOf(func(this js.Value, args []js.Value) any {
 		if !args[0].InstanceOf(js.Global().Get("Uint8Array")) {
 			panic("The first parameter should be an array")
 		}
 		dst := make([]byte, args[0].Length())
 		js.CopyBytesToGo(dst, args[0])
-		records := internal.CastRecordsFromBytes[uint32](dst)
-		heapfast.SortMax(records)
+		records := internal.CastRecordsFromBytes[int32](dst)
+		heap := heapfast.BuildMaxHeap(records)
+		heap.Sort()
+		dst = internal.CastRecordsToBytes(records)
+		js.CopyBytesToJS(args[0], dst)
+		return len(records)
+	}))
+	js.Global().Set("HeapsortDesc", js.FuncOf(func(this js.Value, args []js.Value) any {
+		if !args[0].InstanceOf(js.Global().Get("Uint8Array")) {
+			panic("The first parameter should be an array")
+		}
+		dst := make([]byte, args[0].Length())
+		js.CopyBytesToGo(dst, args[0])
+		records := internal.CastRecordsFromBytes[int32](dst)
+		heap := heapfast.BuildMinHeap(records)
+		heap.Sort()
 		dst = internal.CastRecordsToBytes(records)
 		js.CopyBytesToJS(args[0], dst)
 		return len(records)
